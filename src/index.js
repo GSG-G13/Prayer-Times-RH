701440d061099e4df56bd601b0a9c47cd8166381
raghdarizq <raghdarizq2@gmail.com>
2023-03-29 21:06:57 +0300

    show data in dom  > Relates #16

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ const app = require('./app');
 
 app.listen(app.get('port'), () => {
   // eslint-disable-next-line no-console
-  console.log('App running on port', app.get('port'));
+  console.log('App running on port ', app.get('port'));
 });
