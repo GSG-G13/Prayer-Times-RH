c56e4140b9f4beb226b2a1a620fcf72dcfb8dd29
Heba-Adwan <hebawael1575@gmail.com>
2023-03-29 12:05:24 +0300

    fix-homapag

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const router = require('./contollers/index');
 
 const app = express();
 app.disable('x-powered-by');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 app.use(
   express.static(path.join(__dirname,  '..', 'public'), { maxAge: '30d' }),
 );
