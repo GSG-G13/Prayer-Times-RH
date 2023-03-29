a64d0f4df15e12cded0e34f2173b2c60784ae693
Heba-Adwan <hebawael1575@gmail.com>
2023-03-29 13:32:37 +0300

    fetch-data Relates #11

--- src/contollers/index.js
+++ src/contollers/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const {getData} = require('../contollers/homePage')
 const path = require('path');
 
 
 const router = express.Router();
-const getData = require('./homePage');
 
 router.get('/', (req,res)=>{
   res.sendFile(path.join(__dirname, '..', '..', 'public','index.html'));
 });
-router.get('/search', getData);
+router.get('/search/:name', getData); 
 
 module.exports = router;
